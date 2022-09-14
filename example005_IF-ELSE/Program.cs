bc108fcbaac2dc39398ba90ca914283472d2f99a
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-14 14:22:30 +0300

    Hello, user

---
+++ example005_IF-ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "лина")
+{
+    Console.WriteLine("Урааааа, Это же Лина!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(userName);
+}
+
